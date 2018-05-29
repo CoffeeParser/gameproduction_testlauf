23a1d8147083cffe10cc9e30d35a3b2d481faa13
Denis Dik <denis.dik@hs-furtwangen.de>
2018-05-29 23:23:06 +0200

    Minimap v1 hinzugefugt
    Minimap v1 fenster oben rechts.

---
+++ TestLaufUnityFolder/Assets/Minimap/MinimapFollowPlayerRotation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapFollowPlayerRotation : MonoBehaviour
+{
+
+    public Transform PlayerCameraTransform; // this is the transform where the minimap will follow along the euler-y
+
+    private float xRotLookDown = 90f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.eulerAngles = new Vector3(xRotLookDown, PlayerCameraTransform.eulerAngles.y, 0);
+	}
+}
--- TestLaufUnityFolder/Assets/MobileInputManager.cs
+++ TestLaufUnityFolder/Assets/MobileInputManager.cs
@@ -40,14 +40,24 @@ public class MobileInputManager : MonoBehaviour {
         return false;
     }
 
+    public float speedH = 2.0f;
+    public float speedV = 2.0f;
+
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
     private void Update()
     {
+#if UNITY_EDITOR
+        yaw += speedH * Input.GetAxis("Mouse X");
+        pitch -= speedV * Input.GetAxis("Mouse Y");
 
+        transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+#else
         if (gyro.enabled)
         {
             transform.localRotation = gyro.attitude * rot;
         }
-        
+#endif
 
 
 
